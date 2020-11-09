a226a572def421ce0302d7812692277f552f6126
nauval66 <naufalmuzaky00@gmail.com>
2020-11-09 18:41:23 +0700

    Update Folder

---
+++ Cari 1 angka terbesar.py
@@ -0,0 +1,10 @@
+a=int(input("Masukkan Angka A : "))
+b=int(input("Masukkan Angka B : "))
+c=int(input("Masukkan Angka C : "))
+
+if a > b and a > c:
+    print(a,"Terbesar dari 3 bilangan yang diinputkan")
+elif b > a and b > c:
+     print(b,"Terbesar dari 3 bilangan yang diinputkan")
+else:
+    print(c,"Terbesar dari 3 bilangan yang diinputkan")
